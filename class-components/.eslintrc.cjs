02286bcbb7ca85e658557b3fd01f879f3f8fccac
LinderJK <slepchenkovia@gmail.com>
2024-07-02 22:58:10 +0300

    feat: add husky, eslint, prettier, update package.json scripts

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -4,15 +4,21 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', '@typescript-eslint',  'react-compiler',  'prettier'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-compiler/react-compiler': 'error',
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+
 }
