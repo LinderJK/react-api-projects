ddd626d37f13c3bbbb5408ad18353e468da6e5dd
LinderJK <slepchenkovia@gmail.com>
2024-07-03 21:36:15 +0300

    feat: add ResultBar.tsx and SearchBar.tsx

---
+++ class-components/src/components/ResultBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class ResultBar extends React.Component {}
+
+export default ResultBar;
---
+++ class-components/src/components/SearchBar.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+class SearchBar extends React.Component {}
+export default SearchBar;
