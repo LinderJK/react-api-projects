bd073d1ae767d2e69cdb8c93690e209d2b382745
LinderJK <slepchenkovia@gmail.com>
2024-07-03 21:58:44 +0300

    feat: fetch method, state app, mount effect

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,9 +1,32 @@
 import { Component } from 'react';
 import './App.css';
+import ResultBar from './components/ResultBar/ResultBar.tsx';
 
 class App extends Component {
+    state = {
+        results: [],
+    };
+    async componentDidMount() {
+        const searchQuery = localStorage.getItem('searchQuery') || '';
+        await this.fetchResults(searchQuery);
+    }
+
+    private async fetchResults(searchQuery: string) {
+        try {
+            const data = await fetch(`https://rickandmortyapi.com/api/character/${searchQuery}`);
+            this.setState({ results: data });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     render() {
-        return <div>Hello finder</div>;
+        return (
+            <div>
+                <h1>Hello finder</h1>
+                <ResultBar results={this.state.results}></ResultBar>
+            </div>
+        );
     }
 }
 
