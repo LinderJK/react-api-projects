0b5eb985eb1613af5f40e273204bbc751c5b398f
LinderJK <slepchenkovia@gmail.com>
2024-07-06 01:37:11 +0300

    feat: add fetch error, props and styles

--- class-components/src/components/ResultBar/ResultBar.tsx
+++ class-components/src/components/ResultBar/ResultBar.tsx
@@ -5,14 +5,18 @@ import styles from './ResultBar.module.css';
 
 interface ResultsProps {
     results: Character[];
+    error: string | null;
 }
 
 class ResultBar extends React.Component<ResultsProps> {
     render() {
-        const { results } = this.props;
+        const { results, error } = this.props;
+        if (error) {
+            return <h2 className={styles.error}>{error}</h2>;
+        }
         return (
             <div style={{ margin: '20px' }} className={styles.resultBar}>
-                {results.length > 0 ? (
+                {results && results.length > 0 ? (
                     results.map((result) => <CharacterCard key={result.id} result={result}></CharacterCard>)
                 ) : (
                     <p>No results found.</p>
