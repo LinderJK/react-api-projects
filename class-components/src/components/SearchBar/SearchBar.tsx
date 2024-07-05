23c6e4ab8de6173dcfe0fb41a9da403f867ff84b
LinderJK <slepchenkovia@gmail.com>
2024-07-06 00:04:14 +0300

    feat: add SearchBar.tsx component, add styles

--- class-components/src/components/SearchBar/SearchBar.tsx
+++ class-components/src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,42 @@
 import React from 'react';
+import styles from './SearchBar.module.css';
 
-class SearchBar extends React.Component {}
+interface SearchBarProps {
+    query: string;
+    onSearch: (query: string) => void;
+}
+
+class SearchBar extends React.Component<SearchBarProps> {
+    state = {
+        query: this.props.query,
+    };
+
+    componentDidUpdate(prevProps: SearchBarProps) {
+        if (prevProps.query !== this.props.query) {
+            this.setState({ query: this.props.query });
+        }
+    }
+
+    handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ query: event.target.value.trim() });
+    };
+
+    handleSearch = () => {
+        this.props.onSearch(this.state.query);
+    };
+
+    render() {
+        return (
+            <div className={styles.searchBar}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    value={this.state.query}
+                    onChange={this.handleInputChange}
+                />
+                <button onClick={this.handleSearch}>Search</button>
+            </div>
+        );
+    }
+}
 export default SearchBar;
