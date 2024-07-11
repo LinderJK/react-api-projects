e516d5a3b9a800ed9c699280005634903a83cf07
LinderJK <slepchenkovia@gmail.com>
2024-07-11 20:43:58 +0300

    feat: change to function component

--- class-components/src/components/TitleBar/TitleBar.tsx
+++ class-components/src/components/TitleBar/TitleBar.tsx
@@ -1,15 +1,23 @@
-import { Component } from 'react';
 import RickLogo from '/portal.svg';
 import styles from './TitleBar.module.css';
 
-class TitleBar extends Component {
-    render() {
-        return (
-            <div className={styles.titleBar}>
-                <img className={styles.logoPortal} src={RickLogo} alt="portal" />
-                <h1>Rick & Morty Character Finder</h1>
-            </div>
-        );
-    }
+export default function TitleBar() {
+    return (
+        <div className={styles.titleBar}>
+            <img className={styles.logoPortal} src={RickLogo} alt="portal" />
+            <h1>Rick & Morty Character Finder</h1>
+        </div>
+    );
 }
-export default TitleBar;
+
+// class TitleBar extends Component {
+//     render() {
+//         return (
+//             <div className={styles.titleBar}>
+//                 <img className={styles.logoPortal} src={RickLogo} alt="portal" />
+//                 <h1>Rick & Morty Character Finder</h1>
+//             </div>
+//         );
+//     }
+// }
+// export default TitleBar;
