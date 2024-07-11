4f1b3f5a3f38429938001c40f62478dfd2bf7a68
LinderJK <slepchenkovia@gmail.com>
2024-07-11 20:43:47 +0300

    feat: change to function component

--- class-components/src/components/CharacterCard/CharacterCard.tsx
+++ class-components/src/components/CharacterCard/CharacterCard.tsx
@@ -1,27 +1,46 @@
-import { Component } from 'react';
 import { Character } from '../../types/Character.ts';
 import styles from './CharacterCard.module.css';
 interface CharacterCardProps {
     result: Character;
 }
-class CharacterCard extends Component<CharacterCardProps> {
-    render() {
-        const { name, gender, image, status, type, species } = this.props.result;
-        return (
-            <div
-                className={`${styles.card} ${status === 'Dead' ? styles.dead : ''} ${status === 'Alive' ? styles.alive : ''}`}
-            >
-                <div className={styles.cardTitle}>{name}</div>
-                <div className={styles.avatar}>
-                    <img className={styles.image} src={image} alt={name}></img>
-                </div>
-                <div>Gender - {gender || '...'}</div>
-                <div>Status - {status || '...'}</div>
-                <div>Species - {species || '...'}</div>
-                <div>Type - {type || '...'}</div>
+
+export default function CharacterCard(props: CharacterCardProps) {
+    const { name, gender, image, status, type, species } = props.result;
+    return (
+        <div
+            className={`${styles.card} ${status === 'Dead' ? styles.dead : ''} ${status === 'Alive' ? styles.alive : ''}`}
+        >
+            <div className={styles.cardTitle}>{name}</div>
+            <div className={styles.avatar}>
+                <img className={styles.image} src={image} alt={name}></img>
             </div>
-        );
-    }
+            <div>Gender - {gender || '...'}</div>
+            <div>Status - {status || '...'}</div>
+            <div>Species - {species || '...'}</div>
+            <div>Type - {type || '...'}</div>
+        </div>
+    );
 }
 
-export default CharacterCard;
+// class CharacterCard extends Component<CharacterCardProps> {
+//     render() {
+//         const { name, gender, image, status, type, species } = this.props.result;
+//         return (
+//             <div
+//                 className={`${styles.card} ${status === 'Dead' ? styles.dead : ''} ${status === 'Alive' ? styles.alive : ''}`}
+//             >
+//                 <div className={styles.cardTitle}>{name}</div>
+//                 <div className={styles.avatar}>
+//                     <img className={styles.image} src={image} alt={name}></img>
+//                 </div>
+//                 <div>Gender - {gender || '...'}</div>
+//                 <div>Status - {status || '...'}</div>
+//                 <div>Species - {species || '...'}</div>
+//                 <div>Type - {type || '...'}</div>
+//             </div>
+//         );
+//     }
+// }
+//
+//
+// export default CharacterCard;
