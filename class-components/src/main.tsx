8d5fe1de9a38c548ca18107cb2ecd49297250214
LinderJK <slepchenkovia@gmail.com>
2024-07-11 22:02:04 +0300

    feat: add browserRouter refactor: add errorBoundary

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <ErrorBoundary fallback={<h1>Oh no an error occurred!</h1>}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ErrorBoundary>
     </React.StrictMode>,
 );
