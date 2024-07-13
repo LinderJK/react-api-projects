11274eb3c6b7ef95a1154426d7cf73f70d2c191e
LinderJK <slepchenkovia@gmail.com>
2024-07-14 00:54:15 +0300

    feat: add outlet, add handle close then click on left side menu

--- class-components/src/pages/main/MainPage.tsx
+++ class-components/src/pages/main/MainPage.tsx
@@ -3,7 +3,7 @@ import { useApiRequest } from '../../hooks/useApiRequest.ts';
 import TitleBar from '../../components/TitleBar/TitleBar.tsx';
 import SearchBar from '../../components/SearchBar/SearchBar.tsx';
 import ResultBar from '../../components/ResultBar/ResultBar.tsx';
-import { useSearchParams } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import styles from './mainpage.module.css';
 
@@ -12,6 +12,8 @@ function MainPage() {
     const [searchParams, setSearchParams] = useSearchParams({ name: query, page: '1' });
     const page = parseInt(searchParams.get('page') || '1', 10);
     const searchQuery = searchParams.get('name') || '';
+    const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         if (!searchParams.get('page')) {
@@ -37,27 +39,36 @@ function MainPage() {
         setSearchParams({ name: searchQuery, page: prevPage.toString() });
     };
 
+    const handleClose = () => {
+        if (location.pathname.startsWith('/details/')) {
+            navigate(`/?name=${searchQuery}&page=${page}`);
+        }
+    };
+
     return (
-        <>
-            <div className={'header'}>
-                <TitleBar />
-                <SearchBar onSearch={handleSearch} query={query} />
-            </div>
-            {!error && pages && (
-                <div className={styles.pagination}>
-                    <button onClick={handlePreviousPage} disabled={page === 1}>
-                        Previous
-                    </button>
-
-                    {pages && <div>{`${page} / ${pages}`}</div>}
-
-                    <button onClick={handleNextPage} disabled={page === pages}>
-                        Next
-                    </button>
+        <div className={styles.main}>
+            <div className={styles.left} onClick={handleClose}>
+                <div className={styles.header}>
+                    <TitleBar />
+                    <SearchBar onSearch={handleSearch} query={query} />
                 </div>
-            )}
-            <ResultBar results={data} error={error} isLoading={loading}></ResultBar>
-        </>
+                {!error && pages && (
+                    <div className={styles.pagination}>
+                        <button onClick={handlePreviousPage} disabled={page === 1}>
+                            Previous
+                        </button>
+
+                        {pages && <div>{`${page} / ${pages}`}</div>}
+
+                        <button onClick={handleNextPage} disabled={page === pages}>
+                            Next
+                        </button>
+                    </div>
+                )}
+                <ResultBar results={data} error={error} isLoading={loading}></ResultBar>
+            </div>
+            <Outlet></Outlet>
+        </div>
     );
 }
 
