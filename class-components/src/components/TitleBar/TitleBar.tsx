71f48713684cad1a6f4df23ef1349ffed841e991
LinderJK <slepchenkovia@gmail.com>
2024-07-14 00:56:35 +0300

    feat: add navigate to main page in title

--- class-components/src/components/TitleBar/TitleBar.tsx
+++ class-components/src/components/TitleBar/TitleBar.tsx
@@ -1,9 +1,11 @@
 import RickLogo from '/portal.svg';
 import styles from './TitleBar.module.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function TitleBar() {
+    const navigate = useNavigate();
     return (
-        <div className={styles.titleBar}>
+        <div className={styles.titleBar} onClick={() => navigate('/')}>
             <img className={styles.logoPortal} src={RickLogo} alt="portal" />
             <h1>Rick & Morty Character Finder</h1>
         </div>
