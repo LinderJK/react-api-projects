6462ba044c9e73cb4677ea41047aa154f75b5384
LinderJK <slepchenkovia@gmail.com>
2024-07-14 01:41:21 +0300

    feat: add error and loading in sidebar

--- class-components/src/components/SideBar/SideBar.tsx
+++ class-components/src/components/SideBar/SideBar.tsx
@@ -1,29 +1,45 @@
 import styles from './styles.module.css';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
 import { useEffect, useState } from 'react';
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import { Character } from '../../types/Character.ts';
 
 export default function SideBar() {
     const [data, setData] = useState<Character>();
+    const [error, setError] = useState<string | null>();
+    const [loading, setLoading] = useState<boolean>(false);
     const { id } = useParams();
     console.log(id, 'ID');
 
     useEffect(() => {
         const getSidebarData = async () => {
+            setLoading(true);
             try {
                 const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
                 console.log(response, 'response');
                 if (response.data) {
                     setData(response.data);
                 }
-            } catch (err) {
-                console.log(err);
+            } catch (error: unknown) {
+                if (isAxiosError(error)) {
+                    setError(error.response?.data.error || 'An error occurred');
+                } else {
+                    setError('An unexpected error occurred');
+                    console.error(error);
+                }
+            } finally {
+                setLoading(false);
             }
         };
         getSidebarData();
     }, [id]);
 
-    return <div className={styles.detailsContainer}>{data && <CharacterCard character={data} />}</div>;
+    return (
+        <div className={styles.detailsContainer}>
+            {error && <div>{error}</div>}
+            {loading && <div>loading...</div>}
+            {data && <CharacterCard character={data} />}
+        </div>
+    );
 }
