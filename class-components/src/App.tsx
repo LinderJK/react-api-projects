d88a1543c02d5c5d8c9b9817ef22b55d575d6a14
LinderJK <slepchenkovia@gmail.com>
2024-07-13 16:02:21 +0300

    feat: add outlet

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import SearchPage from './pages/SearchPage.tsx';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/main/MainPage.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
+import Details from './components/Details/Details.tsx';
 
 export default function App() {
     return (
         <div className={'app'}>
             <Routes>
-                <Route path="/" element={<Navigate to={`/search`} />}></Route>
-                <Route path="/search" element={<SearchPage />}></Route>
-                <Route path="*" element={<NotFoundPage />}></Route>
+                <Route path="/" element={<Details />}>
+                    <Route index element={<MainPage />}></Route>
+                    <Route path="*" element={<NotFoundPage />}></Route>
+                </Route>
             </Routes>
         </div>
     );
