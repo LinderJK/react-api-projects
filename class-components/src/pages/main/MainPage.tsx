dbef1714ba48f3f91ea7268b78dda648284d54c1
LinderJK <slepchenkovia@gmail.com>
2024-07-20 17:34:01 +0300

    refactor: delete unused code fix: pagination

--- class-components/src/pages/main/MainPage.tsx
+++ class-components/src/pages/main/MainPage.tsx
@@ -5,44 +5,45 @@ import ResultBar from '../../components/ResultBar/ResultBar.tsx';
 import { Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import styles from './mainpage.module.css';
+import Pagination from '../../components/Pagination/Pagination.tsx';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux.ts';
+import { setSearchQuery } from '../../store/reducers/SearchSlice.ts';
 
 function MainPage() {
+    const dispatch = useAppDispatch();
+
     const { query } = useLocalStorage();
-    const [searchParams, setSearchParams] = useSearchParams({ name: query, page: '1' });
-    const page = parseInt(searchParams.get('page') || '1', 10);
-    const searchQuery = searchParams.get('name') || '';
+    if (query) {
+        console.log('Search query is ' + query);
+        dispatch(setSearchQuery(query));
+    }
+
+    const { currentPage, searchQuery } = useAppSelector((state) => state.search);
+    const { error } = useAppSelector((state) => state.character);
+    const [searchParams, setSearchParams] = useSearchParams();
+
     const navigate = useNavigate();
     const location = useLocation();
 
+    // useEffect(() => {
+    //     if (query) {
+    //         dispatch(setSearchQuery(query));
+    //     }
+    // }, []);
+
     useEffect(() => {
         if (!searchParams.get('page')) {
-            setSearchParams({ page: '1' });
+            dispatch(setSearchQuery(searchParams.get('name') || ''));
         }
-    }, [searchParams, setSearchParams]);
+    }, [searchParams, dispatch]);
 
-    // const { data, loading, error, pages } = useApiRequest(searchQuery, page);
-
-    // const { data } = characterAPI.useGetAllCharacterQuery('');
-    // console.log(data, 'characterAPI.useGetAllCharacterQuery');
-    // const handleSearch = (query: string) => {
-    //     setQuery(query);
-    //     setSearchParams({ name: query, page: '1' });
-    // };
-    //
-    // const handleNextPage = () => {
-    //     if (pages) {
-    //         const nextPage = page + 1;
-    //         setSearchParams({ name: searchQuery, page: nextPage.toString() });
-    //     }
-    // };
-    // const handlePreviousPage = () => {
-    //     const prevPage = page > 1 ? page - 1 : 1;
-    //     setSearchParams({ name: searchQuery, page: prevPage.toString() });
-    // };
+    useEffect(() => {
+        setSearchParams({ name: searchQuery, page: currentPage.toString() });
+    }, [searchQuery, currentPage, setSearchParams]);
 
     const handleClose = () => {
         if (location.pathname.startsWith('/details/')) {
-            navigate(`/?name=${searchQuery}&page=${page}`);
+            navigate(`/?name=${searchQuery}&page=${currentPage}`);
         }
     };
 
@@ -52,9 +53,9 @@ function MainPage() {
                 <div className={styles.header}>
                     <TitleBar />
                     <SearchBar />
+                    {!error && <Pagination />}
                 </div>
-
-                <ResultBar></ResultBar>
+                <ResultBar searchQuery={searchQuery} currentPage={currentPage}></ResultBar>
             </div>
             <Outlet></Outlet>
         </div>
