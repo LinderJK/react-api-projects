24680d5678faa71f3b39669b9d0205f8531de251
LinderJK <slepchenkovia@gmail.com>
2024-07-20 15:40:15 +0300

    feat: add pagination.module.css , add dispatch

--- class-components/src/components/Pagination/Pagination.tsx
+++ class-components/src/components/Pagination/Pagination.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import styles from './pagination.module.css';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux.ts';
+import { setCurrentPage } from '../../store/reducers/SearchSlice.ts';
 
 const Pagination = () => {
+    const dispatch = useAppDispatch();
+    const { currentPage, maxPages } = useAppSelector((state) => state.search);
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            dispatch(setCurrentPage(currentPage - 1));
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < maxPages) {
+            dispatch(setCurrentPage(currentPage + 1));
+        }
+    };
+
     return (
         <div>
-            {!error && pages && (
+            {maxPages > 0 && (
                 <div className={styles.pagination}>
-                    <button onClick={handlePreviousPage} disabled={page === 1}>
+                    <button onClick={handlePreviousPage} disabled={currentPage === 1}>
                         Previous
                     </button>
 
-                    {pages && <div>{`${page} / ${pages}`}</div>}
+                    <div>{`${currentPage} / ${maxPages}`}</div>
 
-                    <button onClick={handleNextPage} disabled={page === pages}>
+                    <button onClick={handleNextPage} disabled={currentPage === maxPages}>
                         Next
                     </button>
                 </div>
