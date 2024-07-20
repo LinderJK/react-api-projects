6c6daa5c0af50ae43b5b9c7ce549dd928e228894
LinderJK <slepchenkovia@gmail.com>
2024-07-20 15:25:25 +0300

    feat: add provider

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -4,12 +4,16 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ErrorBoundary fallback={<h1>Oh no an error occurred!</h1>}>
             <BrowserRouter>
-                <App />
+                <Provider store={store}>
+                    <App />
+                </Provider>
             </BrowserRouter>
         </ErrorBoundary>
     </React.StrictMode>,
