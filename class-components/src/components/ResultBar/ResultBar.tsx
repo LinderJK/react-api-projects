5e1df90b813f628f315df34dfd349ed575937502
LinderJK <slepchenkovia@gmail.com>
2024-07-20 15:26:36 +0300

    feat: add rtk query hooks

--- class-components/src/components/ResultBar/ResultBar.tsx
+++ class-components/src/components/ResultBar/ResultBar.tsx
@@ -1,18 +1,28 @@
-import { Character } from '../../types/Character.ts';
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import styles from './ResultBar.module.css';
+import { characterAPI } from '../../services/CharacterService.ts';
 
-interface ResultsProps {
-    results: Character[];
-    error: string | null;
-    isLoading: boolean;
+interface IResultBarProps {
+    searchQuery: string;
+    currentPage: number;
 }
 
-export default function ResultBar(props: ResultsProps) {
-    const { results, error, isLoading } = props;
+export default function ResultBar({ searchQuery, currentPage }: IResultBarProps) {
+    // const dispatch = useAppDispatch();
+    const { data, isLoading, error } = characterAPI.useGetCharactersByPageQuery({
+        page: currentPage,
+        name: searchQuery,
+    });
 
+    // useEffect(() => {
+    //     if (data?.results) {
+    //         dispatch(setCharacters(data.results));
+    //     }
+    // }, [data, dispatch]);
+
+    console.log(data, 'character query loaded');
     if (error) {
-        return <h2 className={styles.error}>{error}</h2>;
+        return <h2 className={styles.error}>{'An error occurred'}</h2>;
     }
     if (isLoading) {
         return <p>Loading...</p>;
@@ -20,8 +30,8 @@ export default function ResultBar(props: ResultsProps) {
 
     return (
         <div style={{ margin: '20px' }} className={styles.resultBar}>
-            {results && results.length > 0 ? (
-                results.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
+            {!!data?.results && data.results.length > 0 ? (
+                data.results.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
             ) : (
                 <p>No results found.</p>
             )}
