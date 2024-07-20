e72424c7480b51a245c8beece502cdc3c23c72df
LinderJK <slepchenkovia@gmail.com>
2024-07-20 15:24:31 +0300

    feat: add character slice

---
+++ class-components/src/store/reducers/CharacterSlice.ts
@@ -0,0 +1,61 @@
+import { Character } from '../../types/Character.ts';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { characterAPI } from '../../services/CharacterService.ts';
+
+export interface ICharacterState {
+    characters: Character[];
+    isLoading: boolean;
+    error: Error | string;
+    selectedIds: Set<number>;
+    isDetailsOpen: boolean;
+}
+
+const initialState: ICharacterState = {
+    characters: [],
+    isLoading: false,
+    error: '',
+    selectedIds: new Set(),
+    isDetailsOpen: false,
+};
+
+export const characterSlice = createSlice({
+    name: 'character',
+    initialState,
+    reducers: {
+        setCharacters(state, action: PayloadAction<Character[]>) {
+            state.characters = action.payload;
+        },
+        selectCharacter(state, action: PayloadAction<number>) {
+            state.selectedIds.add(action.payload);
+        },
+        unselectCharacter(state, action: PayloadAction<number>) {
+            state.selectedIds.delete(action.payload);
+        },
+        // selectAllCharacter(state, action: PayloadAction<Character>) {
+        //     state.selected.push(action.payload);
+        // },
+        // deleteCharacter(state, action: PayloadAction<Character>) {
+        //     state.selected = state.selected.filter((item) => item.id !== action.payload.id);
+        // },
+        // deleteAllCharacter(state) {
+        //     state.selected = [];
+        // },
+    },
+    extraReducers: (builder) => {
+        builder.addMatcher(characterAPI.endpoints?.getCharactersByPage.matchFulfilled, (state, { payload }) => {
+            state.characters = payload.results;
+            state.isLoading = false;
+        });
+        builder.addMatcher(characterAPI.endpoints?.getCharactersByPage.matchPending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addMatcher(characterAPI.endpoints?.getCharactersByPage.matchRejected, (state, { error }) => {
+            state.isLoading = false;
+            state.error = error.message || 'An error occurred';
+        });
+    },
+});
+
+export const { setCharacters, selectCharacter, unselectCharacter } = characterSlice.actions;
+
+export default characterSlice.reducer;
