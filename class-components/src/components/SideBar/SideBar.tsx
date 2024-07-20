8b2a0faa2eca9ee14bedf7fa0717dfec5af20603
LinderJK <slepchenkovia@gmail.com>
2024-07-20 18:04:51 +0300

    refactor: change logic with rtk query

--- class-components/src/components/SideBar/SideBar.tsx
+++ class-components/src/components/SideBar/SideBar.tsx
@@ -1,42 +1,48 @@
 import styles from './styles.module.css';
 import { useParams } from 'react-router-dom';
-import axios, { isAxiosError } from 'axios';
-import { useEffect, useState } from 'react';
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
-import { Character } from '../../types/Character.ts';
+import { useGetCharacterByIdQuery } from '../../services/CharacterService.ts';
 
 export default function SideBar() {
-    const [data, setData] = useState<Character>();
-    const [error, setError] = useState<string | null>();
-    const [loading, setLoading] = useState<boolean>(false);
+    // const [data, setData] = useState<Character>();
+    // const [error, setError] = useState<string | null>();
+    // const [loading, setLoading] = useState<boolean>(false);
     const { id } = useParams();
+    const { data, error, isLoading } = useGetCharacterByIdQuery(id || '', {
+        skip: !id,
+    });
+    console.log(data, 'DATA FROM SIDE BAR');
 
-    useEffect(() => {
-        const getSidebarData = async () => {
-            setLoading(true);
-            try {
-                const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
-                if (response.data) {
-                    setData(response.data);
-                }
-            } catch (error: unknown) {
-                if (isAxiosError(error)) {
-                    setError(error.response?.data.error || 'An error occurred');
-                } else {
-                    setError('An unexpected error occurred');
-                    console.error(error);
-                }
-            } finally {
-                setLoading(false);
-            }
-        };
-        getSidebarData();
-    }, [id]);
+    if (!id) {
+        return <div className={styles.detailsContainer}>Invalid character ID</div>;
+    }
+
+    // useEffect(() => {
+    //     const getSidebarData = async () => {
+    //         setLoading(true);
+    //         try {
+    //             const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
+    //             if (response.data) {
+    //                 setData(response.data);
+    //             }
+    //         } catch (error: unknown) {
+    //             if (isAxiosError(error)) {
+    //                 setError(error.response?.data.error || 'An error occurred');
+    //             } else {
+    //                 setError('An unexpected error occurred');
+    //                 console.error(error);
+    //             }
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     getSidebarData();
+    // }, [id]);
 
     return (
         <div className={styles.detailsContainer}>
-            {error && <div>{error}</div>}
-            {loading && <div>loading...</div>}
+            {error && <div>{error.toString()}</div>}
+            {isLoading && <div>Loading...</div>}
             {data && <CharacterCard character={data} />}
         </div>
     );
