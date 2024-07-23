5146068aebeeb6fe6546ebdb8db74a2da62d48e7
LinderJK <slepchenkovia@gmail.com>
2024-07-23 19:29:18 +0300

    refactor: add sidebar data in store

--- class-components/src/components/SideBar/SideBar.tsx
+++ class-components/src/components/SideBar/SideBar.tsx
@@ -2,42 +2,27 @@ import styles from './styles.module.css';
 import { useParams } from 'react-router-dom';
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import { useGetCharacterByIdQuery } from '../../services/CharacterService.ts';
+import { useAppDispatch } from '../../hooks/redux.ts';
+import { addDetailsItem } from '../../store/reducers/DetailsSlice.ts';
+import { useEffect } from 'react';
 
 export default function SideBar() {
-    // const [data, setData] = useState<Character>();
-    // const [error, setError] = useState<string | null>();
-    // const [loading, setLoading] = useState<boolean>(false);
     const { id } = useParams();
+    const dispatch = useAppDispatch();
+
     const { data, error, isLoading } = useGetCharacterByIdQuery(id || '', {
         skip: !id,
     });
-    console.log(data, 'DATA FROM SIDE BAR');
+
+    useEffect(() => {
+        dispatch(addDetailsItem(data));
+    }, [data, dispatch]);
 
     if (!id) {
         return <div className={styles.detailsContainer}>Invalid character ID</div>;
     }
 
-    // useEffect(() => {
-    //     const getSidebarData = async () => {
-    //         setLoading(true);
-    //         try {
-    //             const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
-    //             if (response.data) {
-    //                 setData(response.data);
-    //             }
-    //         } catch (error: unknown) {
-    //             if (isAxiosError(error)) {
-    //                 setError(error.response?.data.error || 'An error occurred');
-    //             } else {
-    //                 setError('An unexpected error occurred');
-    //                 console.error(error);
-    //             }
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
-    //     getSidebarData();
-    // }, [id]);
+    console.log(data, 'character ID is valid DATA!!!!');
 
     return (
         <div className={styles.detailsContainer}>
