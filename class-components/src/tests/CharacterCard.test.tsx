47863a06ca16f2e45e057e01d295eabff116bad8
LinderJK <slepchenkovia@gmail.com>
2024-07-25 23:13:02 +0300

    feat: add handlers from mock service worker, setup server

--- class-components/src/tests/CharacterCard.test.tsx
+++ class-components/src/tests/CharacterCard.test.tsx
@@ -1,38 +1,53 @@
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { afterEach, describe, expect, test } from 'vitest';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import CharacterCard from '../components/CharacterCard/CharacterCard';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import DetailsPage from '../pages/DetailsPage/DetailsPage';
+import { delay, http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+import { renderWithProviders } from './testStore/renderWithProviders.tsx';
 
-const data = {
-    id: 1,
-    name: 'Rick',
-    status: 'Dead',
-    species: 'Human',
-    type: 'Type 1',
-    gender: 'Male',
-    origin: {
-        name: 'Origin 1',
-        url: 'https://example.com/origin-1',
-    },
-    image: 'https://example.com/image-1',
-    episode: ['https://example.com/episode-1', 'https://example.com/episode-2'],
-    url: 'https://example.com/character-1',
-    created: 1234567890,
-};
+// const data = {
+//     id: 1,
+//     name: 'Rick',
+//     status: 'Dead',
+//     species: 'Human',
+//     type: 'Type 1',
+//     gender: 'Male',
+//     origin: {
+//         name: 'Origin 1',
+//         url: 'https://example.com/origin-1',
+//     },
+//     image: 'https://example.com/image-1',
+//     episode: ['https://example.com/episode-1', 'https://example.com/episode-2'],
+//     url: 'https://example.com/character-1',
+//     created: 1234567890,
+// };
+
+export const handlers = [
+    http.get('/api/user', async () => {
+        await delay(150);
+        return HttpResponse.json();
+    }),
+];
+
+const server = setupServer(...handlers);
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('CharacterCard Component', () => {
-    beforeEach(() => {
-        vi.resetAllMocks();
-    });
-
-    afterEach(() => {
-        vi.clearAllMocks();
-    });
+    // beforeEach(() => {
+    //     vi.resetAllMocks();
+    // });
+    //
+    // afterEach(() => {
+    //     vi.clearAllMocks();
+    // });
 
     test('renders the CharacterCard component with title and image', () => {
-        render(
-            <MemoryRouter>
+        renderWithProviders(
+            <MemoryRouter initialEntries={['/']}>
                 <CharacterCard character={data} />
             </MemoryRouter>,
         );
