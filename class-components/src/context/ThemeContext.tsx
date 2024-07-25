e85ad08f0d9c187859bb76f47800e0c955fc3018
LinderJK <slepchenkovia@gmail.com>
2024-07-25 21:25:18 +0300

    feat: add context theme, add hook useTheme

---
+++ class-components/src/context/ThemeContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, ReactNode, useState } from 'react';
+
+type Theme = 'light' | 'dark';
+
+export interface ThemeContextType {
+    theme: Theme;
+    toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const [theme, setTheme] = useState<Theme>('dark');
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
+    return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
