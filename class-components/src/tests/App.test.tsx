fee9011aeea690a29501c89c23d1f262288aef30
LinderJK <slepchenkovia@gmail.com>
2024-07-27 19:01:18 +0300

    feat: add tests for theme, error button

--- class-components/src/tests/App.test.tsx
+++ class-components/src/tests/App.test.tsx
@@ -1,14 +1,57 @@
-import { render } from '@testing-library/react';
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import { renderWithProviders } from './testStore/renderWithProviders.tsx';
+import { ThemeProvider } from '../context/ThemeContext.tsx';
+import { fireEvent, screen } from '@testing-library/react';
 
-describe('App', () => {
+describe('App test', () => {
     test('renders the App component', () => {
-        render(
+        renderWithProviders(
             <MemoryRouter>
                 <App />
             </MemoryRouter>,
         );
     });
+
+    test('toggle theme', () => {
+        renderWithProviders(
+            <MemoryRouter>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </MemoryRouter>,
+        );
+
+        const button = screen.getByText(/Switch to Light/i);
+        expect(button).toBeInTheDocument();
+
+        fireEvent.click(button);
+
+        expect(button).toHaveTextContent(/Switch to Dark/i);
+
+        fireEvent.click(button);
+        expect(button).toHaveTextContent(/Switch to Light/i);
+    });
+    test('checks the error'),
+        () => {
+            renderWithProviders(
+                <MemoryRouter initialEntries={['/']}>
+                    <App />
+                </MemoryRouter>,
+            );
+
+            const error = screen.getByText('Error!');
+            const search = screen.getByPlaceholderText('Input Character Name');
+            expect(search).toBeInTheDocument();
+            expect(error).toBeInTheDocument();
+
+            fireEvent.click(error);
+            const errorMsg = screen.getByText('Oops! Render Error :(');
+            expect(errorMsg).toBeInTheDocument();
+            const buttonClose = screen.getByText('Retry');
+            fireEvent.click(buttonClose);
+            expect(errorMsg).not.toBeInTheDocument();
+            expect(search).toBeInTheDocument();
+        };
 });
