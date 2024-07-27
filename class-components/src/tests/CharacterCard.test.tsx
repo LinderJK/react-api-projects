7d1caef9d6aa052afbaaeeab5e799194eab605c8
LinderJK <slepchenkovia@gmail.com>
2024-07-27 19:02:44 +0300

    feat: add tests for CharacterCard.test.tsx preload state, render component, navigate, checkbox

--- class-components/src/tests/CharacterCard.test.tsx
+++ class-components/src/tests/CharacterCard.test.tsx
@@ -1,49 +1,46 @@
-import { afterEach, describe, expect, test } from 'vitest';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import CharacterCard from '../components/CharacterCard/CharacterCard';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import DetailsPage from '../pages/DetailsPage/DetailsPage';
-import { delay, http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node';
 import { renderWithProviders } from './testStore/renderWithProviders.tsx';
 
-// const data = {
-//     id: 1,
-//     name: 'Rick',
-//     status: 'Dead',
-//     species: 'Human',
-//     type: 'Type 1',
-//     gender: 'Male',
-//     origin: {
-//         name: 'Origin 1',
-//         url: 'https://example.com/origin-1',
-//     },
-//     image: 'https://example.com/image-1',
-//     episode: ['https://example.com/episode-1', 'https://example.com/episode-2'],
-//     url: 'https://example.com/character-1',
-//     created: 1234567890,
-// };
+const data = {
+    id: 1,
+    name: 'Rick',
+    status: 'Dead',
+    species: 'Human',
+    type: 'Type 1',
+    gender: 'Male',
+    origin: {
+        name: 'Origin 1',
+        url: 'https://example.com/origin-1',
+    },
+    image: 'https://example.com/image-1',
+    episode: ['https://example.com/episode-1', 'https://example.com/episode-2'],
+    url: 'https://example.com/character-1',
+    created: 1234567890,
+};
 
-export const handlers = [
-    http.get('/api/user', async () => {
-        await delay(150);
-        return HttpResponse.json();
-    }),
-];
-
-const server = setupServer(...handlers);
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-describe('CharacterCard Component', () => {
-    // beforeEach(() => {
-    //     vi.resetAllMocks();
-    // });
-    //
-    // afterEach(() => {
-    //     vi.clearAllMocks();
-    // });
+describe('CharacterCard Component test', () => {
+    test('Preloaded state to render', () => {
+        renderWithProviders(
+            <MemoryRouter>
+                <CharacterCard character={data} />
+            </MemoryRouter>,
+            {
+                preloadedState: {
+                    favorite: {
+                        selected: [data],
+                        isLoading: false,
+                        error: '',
+                    },
+                },
+            },
+        );
+        const checkbox = screen.getByRole('checkbox');
+        expect(checkbox).toBeChecked();
+    });
 
     test('renders the CharacterCard component with title and image', () => {
         renderWithProviders(
@@ -56,11 +53,13 @@ describe('CharacterCard Component', () => {
         const imageElement = screen.getByAltText(data.name);
         expect(imageElement).toBeInTheDocument();
         expect(imageElement).toHaveAttribute('src', data.image);
+        const checkbox = screen.getByRole('checkbox');
+        expect(checkbox).toBeInTheDocument();
     });
 
-    test('Переходит на страницу с подробной информацией при клике на карточку', async () => {
-        render(
-            <MemoryRouter>
+    test('navigates to DetailsPage', async () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={['/']}>
                 <Routes>
                     <Route path={`/`} element={<CharacterCard character={data} />} />
                     <Route path={`/details/${data.id}`} element={<DetailsPage />} />
@@ -68,30 +67,41 @@ describe('CharacterCard Component', () => {
             </MemoryRouter>,
         );
 
-        const card = screen.getByText(data.name);
-        fireEvent.click(card);
+        const imageElement = screen.getByAltText(data.name);
+        fireEvent.click(imageElement);
 
         await waitFor(() => {
             const detailsHeading = screen.getByRole('heading', { name: 'Details' });
             expect(detailsHeading).toBeInTheDocument();
         });
     });
-
-    test('renders the DetailsPage component with "Details"', async () => {
-        render(
-            <MemoryRouter initialEntries={[`/details/${data.id}`]}>
-                <Routes>
-                    <Route path="/details/:id" element={<DetailsPage />} />
-                </Routes>
+    test('checks the checkbox', async () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={['/']}>
+                <CharacterCard character={data} />
             </MemoryRouter>,
         );
-
+        const checkbox = screen.getByRole('checkbox');
+        fireEvent.click(checkbox);
         await waitFor(() => {
-            const nameElement = screen.getByText(data.name);
-            expect(nameElement).toBeInTheDocument();
-            const imageElement = screen.getByAltText(data.name);
-            expect(imageElement).toBeInTheDocument();
-            expect(imageElement).toHaveAttribute('src', data.image);
+            expect(checkbox).toBeChecked();
+        });
+    });
+
+    test('unchecks the checkbox', async () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={['/']}>
+                <CharacterCard character={data} />
+            </MemoryRouter>,
+        );
+        const checkbox = screen.getByRole('checkbox');
+        fireEvent.click(checkbox);
+        await waitFor(() => {
+            expect(checkbox).toBeChecked();
+        });
+        fireEvent.click(checkbox);
+        await waitFor(() => {
+            expect(checkbox).not.toBeChecked();
         });
     });
 });
