bb447a2818b5dd6e0ad44b81bf4a9a7cf818c13d
LinderJK <slepchenkovia@gmail.com>
2024-07-28 19:19:15 +0300

    refactor: add rtk query hook, add refetch fn

--- class-components/src/components/ResultBar/ResultBar.tsx
+++ class-components/src/components/ResultBar/ResultBar.tsx
@@ -1,32 +1,36 @@
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import styles from './ResultBar.module.css';
-// import { characterAPI } from '../../services/CharacterService.ts';
-import { Character } from '../../types/Character.ts';
+import { useGetCharactersByPageQuery } from '../../services/CharacterService.ts';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-interface IResultBarProps {
-    character: Character[];
-}
+export default function ResultBar() {
+    const router = useRouter();
+    const { query } = router;
+    const { name, page } = query;
 
-export default function ResultBar({ character }: IResultBarProps) {
-    // const { data, isLoading, error } = characterAPI.useGetCharactersByPageQuery({
-    //     page: currentPage,
-    //     name: searchQuery,
-    // });
+    const searchQuery = (name ?? '') as string;
+    const currentPage = parseInt(page as string, 10) || 1;
+    const { data, isLoading, error, refetch } = useGetCharactersByPageQuery({
+        page: currentPage,
+        name: searchQuery,
+    });
 
-    // if (error) {
-    //     return <h2 className={styles.error}>{'Results not found'}</h2>;
-    // }
-    // if (isLoading) {
-    //     return <p>Loading...</p>;
-    // }
-    // if (!character) {
-    //     return <p>Loading...</p>;
-    // }
+    useEffect(() => {
+        refetch();
+    }, [query, refetch]);
+
+    if (error) {
+        return <h2 className={styles.error}>{'Results not found'}</h2>;
+    }
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <div style={{ margin: '20px' }} className={styles.resultBar}>
-            {!!character && character.length > 0 ? (
-                character.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
+            {!!data && data.results.length > 0 ? (
+                data.results.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
             ) : (
                 <p>No results found.</p>
             )}
