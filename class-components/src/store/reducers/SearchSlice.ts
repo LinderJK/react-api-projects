686ed1eaf9d83e485abbe76d88c4b43985aeee84
LinderJK <slepchenkovia@gmail.com>
2024-07-28 19:20:09 +0300

    refactor: delete unused reducers

--- class-components/src/store/reducers/SearchSlice.ts
+++ class-components/src/store/reducers/SearchSlice.ts
@@ -1,36 +1,23 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { characterAPI } from '../../services/CharacterService.ts';
 
 export interface ISearchState {
-    searchQuery: string;
-    currentPage: number;
     maxPages: number;
 }
 
 const initialState: ISearchState = {
-    searchQuery: '',
-    currentPage: 1,
     maxPages: 0,
 };
 
 export const searchSlice = createSlice({
     name: 'search',
     initialState,
-    reducers: {
-        setSearchQuery(state, action: PayloadAction<string>) {
-            state.searchQuery = action.payload;
-        },
-        setCurrentPage(state, action: PayloadAction<number>) {
-            state.currentPage = action.payload;
-        },
-    },
+    reducers: {},
     extraReducers(builder) {
         builder.addMatcher(characterAPI.endpoints?.getCharactersByPage.matchFulfilled, (state, { payload }) => {
             state.maxPages = payload.info.pages;
         });
     },
 });
-
-export const { setSearchQuery, setCurrentPage } = searchSlice.actions;
 
 export default searchSlice.reducer;
