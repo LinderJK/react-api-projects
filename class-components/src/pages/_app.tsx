7a91edbf6ac5d6b623dfec24486d9ebbc5d8b6df
LinderJK <slepchenkovia@gmail.com>
2024-07-28 19:13:53 +0300

    feat: add getLayout fn, add errorBoundary, add fallBack component

--- class-components/src/pages/_app.tsx
+++ class-components/src/pages/_app.tsx
@@ -2,18 +2,37 @@ import '../styles/globals.css';
 import '../styles/theme.css';
 import { Provider } from 'react-redux';
 import { AppProps } from 'next/app';
-import { store } from '../store/store.ts';
-import Layout from './layout.tsx';
 import { ThemeProvider } from '../context/ThemeContext.tsx';
+import { store } from '../store/store.ts';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary.tsx';
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = NonNullable<unknown>, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page);
     return (
-        <Provider store={store}>
-            <ThemeProvider>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </ThemeProvider>
-        </Provider>
+        <ErrorBoundary fallback={fallBackComponent()}>
+            <Provider store={store}>
+                <ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+            </Provider>
+        </ErrorBoundary>
     );
 }
+
+const fallBackComponent = () => {
+    return (
+        <>
+            <h1>Oh no an error occurred!</h1>
+            <p>This error from ErrorBoundary</p>
+            <p>Please reload the page =)</p>
+        </>
+    );
+};
