0d8dab46259ca565ae29b5bc303d8c02fd4f17fb
LinderJK <slepchenkovia@gmail.com>
2024-07-28 14:00:30 +0300

    refactor: move old components to folder old

---
+++ class-components/old/main.tsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+// import './globals.css';
+// import { BrowserRouter } from 'react-router-dom';
+// import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+// import { Provider } from 'react-redux';
+// import { store } from './store/store.ts';
+// import { ThemeProvider } from './context/ThemeContext.tsx';
+//
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//         <ErrorBoundary fallback={<h1>Oh no an error occurred!</h1>}>
+//             <BrowserRouter>
+//                 <Provider store={store}>
+//                     <ThemeProvider>
+//                         <App />
+//                     </ThemeProvider>
+//                 </Provider>
+//             </BrowserRouter>
+//         </ErrorBoundary>
+//     </React.StrictMode>,
+// );
