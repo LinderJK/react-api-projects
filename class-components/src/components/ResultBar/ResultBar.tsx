e1d3893405121c0b63e8aab9487a32953485e89e
LinderJK <slepchenkovia@gmail.com>
2024-07-30 23:17:41 +0300

    feat: switch store to server props

--- class-components/src/components/ResultBar/ResultBar.tsx
+++ class-components/src/components/ResultBar/ResultBar.tsx
@@ -1,36 +1,48 @@
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import styles from './ResultBar.module.css';
-import { useGetCharactersByPageQuery } from '../../services/CharacterService.ts';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+// import { useGetCharactersByPageQuery } from '../../services/CharacterService.ts';
+// import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
+// import { useAppSelector } from '../../hooks/redux.ts';
+// import { wrapper } from '../../store/store.ts';
+import { Character } from '../../types/Character.ts';
 
-export default function ResultBar() {
-    const router = useRouter();
-    const { query } = router;
-    const { name, page } = query;
+interface ResultBarProps {
+    results: Character[];
+}
 
-    const searchQuery = (name ?? '') as string;
-    const currentPage = parseInt(page as string, 10) || 1;
-    const { data, isLoading, error, refetch } = useGetCharactersByPageQuery({
-        page: currentPage,
-        name: searchQuery,
-    });
+export default function ResultBar({ results }: ResultBarProps) {
+    // const router = useRouter();
+    // const { query } = router;
+    // const { name, page } = query;
+    // const { hydrating } = wrapper.useHydration(props);
 
-    useEffect(() => {
-        refetch();
-    }, [query, refetch]);
+    // const searchQuery = (name ?? '') as string;
+    // const currentPage = parseInt(page as string, 10) || 1;
+    // const { renderData, isLoading, error, refetch } = useGetCharactersByPageQuery({
+    //     page: currentPage,
+    //     name: searchQuery,
+    // });
 
-    if (error) {
-        return <h2 className={styles.error}>{'Results not found'}</h2>;
-    }
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
+    // const { characters, isLoading, error, isDetailsOpen } = useAppSelector((state) => state.character);
+    // console.log(characters);
+
+    // useEffect(() => {
+    //     refetch();
+    // }, [query, refetch]);
+
+    // if (error) {
+    //     return <h2 className={styles.error}>{'Results not found'}</h2>;
+    // }
+    // if (isLoading) {
+    //     return <p>Loading...</p>;
+    // }
+    const renderData = results.length > 0 ? results : null;
 
     return (
         <div style={{ margin: '20px' }} className={styles.resultBar}>
-            {!!data && data.results.length > 0 ? (
-                data.results.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
+            {renderData ? (
+                renderData.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
             ) : (
                 <p>No results found.</p>
             )}
