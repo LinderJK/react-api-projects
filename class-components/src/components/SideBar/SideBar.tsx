c6da40d926ad0756baa5d1fe47a7cd926c0a1bcc
LinderJK <slepchenkovia@gmail.com>
2024-07-30 23:17:50 +0300

    feat: switch store to server props

--- class-components/src/components/SideBar/SideBar.tsx
+++ class-components/src/components/SideBar/SideBar.tsx
@@ -1,24 +1,29 @@
 import styles from './styles.module.css';
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
-import { useGetCharacterByIdQuery } from '../../services/CharacterService.ts';
-import { useAppDispatch } from '../../hooks/redux.ts';
-import { addDetailsItem } from '../../store/reducers/DetailsSlice.ts';
-import { useEffect } from 'react';
+// import { useGetCharacterByIdQuery } from '../../services/CharacterService.ts';
+// import { useAppDispatch } from '../../hooks/redux.ts';
+// import { addDetailsItem } from '../../store/reducers/DetailsSlice.ts';
+// import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+// import { data } from 'happy-dom/lib/PropertySymbol';
+import { Character } from '../../types/Character.ts';
 
-export default function SideBar() {
+interface SideBarProps {
+    dataDetails: Character;
+}
+export default function SideBar({ dataDetails }: SideBarProps) {
     const router = useRouter();
     const { id } = router.query;
     // console.log(router.query, id);
-    const dispatch = useAppDispatch();
+    // const dispatch = useAppDispatch();
 
-    const { data, error, isLoading } = useGetCharacterByIdQuery((id as string) ?? '', {
-        skip: !id,
-    });
+    // const { data, error, isLoading } = useGetCharacterByIdQuery((id as string) ?? '', {
+    //     skip: !id,
+    // });
 
-    useEffect(() => {
-        dispatch(addDetailsItem(data));
-    }, [data, dispatch]);
+    // useEffect(() => {
+    //     dispatch(addDetailsItem(data));
+    // }, [data, dispatch]);
 
     if (!id) {
         return <div className={styles.detailsContainer}>Invalid character ID</div>;
@@ -26,9 +31,9 @@ export default function SideBar() {
 
     return (
         <div className={styles.detailsContainer}>
-            {error && <div>{error.toString()}</div>}
-            {isLoading && <div>Loading...</div>}
-            {data && <CharacterCard character={data} />}
+            {/*{error && <div>{error.toString()}</div>}*/}
+            {/*{isLoading && <div>Loading...</div>}*/}
+            {dataDetails && <CharacterCard character={dataDetails} />}
         </div>
     );
 }
