3ab63eb2fa922513d4a1ae4a3fa262e554e8ee19
LinderJK <slepchenkovia@gmail.com>
2024-07-30 23:15:08 +0300

    fix: change path on link, fix isDetails link

--- class-components/src/components/CharacterCard/CharacterCard.tsx
+++ class-components/src/components/CharacterCard/CharacterCard.tsx
@@ -12,12 +12,14 @@ interface CharacterCardProps {
 export default function CharacterCard(props: CharacterCardProps) {
     const { name, gender, image, status, type, species, id } = props.character;
     const router = useRouter();
+    const { page, name: queryName } = router.query;
+    // console.log(router, 'ROUTER CHARACTER CARD');
     const { selectCharacter, unselectCharacter } = favoriteSlice.actions;
     const dispatch = useAppDispatch();
     const { selected } = useAppSelector((state) => state.favorite);
 
     const isDetails = (() => {
-        return router.pathname.startsWith('/details/');
+        return router.pathname.includes('/details/');
     })();
 
     const isSelectedCharacter = useCallback(
@@ -40,7 +42,13 @@ export default function CharacterCard(props: CharacterCardProps) {
             className={`${styles.card} ${status === 'Dead' ? styles.dead : ''} ${status === 'Alive' ? styles.alive : ''} ${isDetails ? '' : styles.cardActive}`}
         >
             <div className={styles.cardTitle}>{name}</div>
-            <Link href={`/details/${id}`}>
+            <Link
+                href={{
+                    pathname: `/character/details/${id}`,
+                    query: { page, queryName },
+                }}
+                as={`/character/details/${id}?page=${page}&name=${name}`}
+            >
                 <div className={styles.avatar}>
                     <img className={styles.image} src={image} alt={name}></img>
                 </div>
