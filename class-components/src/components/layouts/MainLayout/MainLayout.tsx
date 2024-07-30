797ed2c104b998d19401afa1581393cc6d7caa91
LinderJK <slepchenkovia@gmail.com>
2024-07-31 00:38:51 +0300

    feat: add favicon and title

--- class-components/src/components/layouts/MainLayout/MainLayout.tsx
+++ class-components/src/components/layouts/MainLayout/MainLayout.tsx
@@ -3,11 +3,16 @@ import ThemeToggle from '../../ThemeToggle/ThemeToggle.tsx';
 import styles from './layout.module.css';
 import { useTheme } from '../../../hooks/useTheme.ts';
 import SearchBar from '../../SearchBar/SearchBar.tsx';
+import Head from 'next/head';
 
 export default function MainLayout({ children }: { children: JSX.Element }) {
     const { theme } = useTheme();
     return (
         <>
+            <Head>
+                <title>Rick and Morty Characters</title>
+                <link rel="icon" href="/rick.svg" type="image/svg+xml" />
+            </Head>
             <div data-theme={theme} className={styles.app}>
                 <div className={styles.container}>
                     <div className={styles.header}>
