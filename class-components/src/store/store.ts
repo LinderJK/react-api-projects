f92a23854a795cb153031e51b4f8a2eb0faedbe1
LinderJK <slepchenkovia@gmail.com>
2024-07-30 22:06:33 +0300

    feat: configure store with redux next wrapper

--- class-components/src/store/store.ts
+++ class-components/src/store/store.ts
@@ -1,22 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
-import characterReducer from './reducers/CharacterSlice.ts';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { characterSlice } from './reducers/CharacterSlice.ts';
 import { characterAPI } from '../services/CharacterService.ts';
-import searchReducer from './reducers/SearchSlice.ts';
-import favoriteReducer from './reducers/FavoriteSlice.ts';
-import detailsReducer from './reducers/DetailsSlice.ts';
+import { searchSlice } from './reducers/SearchSlice.ts';
+import { favoriteSlice } from './reducers/FavoriteSlice.ts';
+import { detailsSlice } from './reducers/DetailsSlice.ts';
+import logger from 'redux-logger';
+import { createWrapper, MakeStore } from 'next-redux-wrapper';
 
-export const store = configureStore({
-    reducer: {
-        character: characterReducer,
-        [characterAPI.reducerPath]: characterAPI.reducer,
-        search: searchReducer,
-        favorite: favoriteReducer,
-        details: detailsReducer,
-    },
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(characterAPI.middleware);
-    },
-});
+const reducers = {
+    [characterSlice.name]: characterSlice.reducer,
+    [characterAPI.reducerPath]: characterAPI.reducer,
+    [searchSlice.name]: searchSlice.reducer,
+    [favoriteSlice.name]: favoriteSlice.reducer,
+    [detailsSlice.name]: detailsSlice.reducer,
+};
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+const reducer = combineReducers(reducers);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const makeStore: MakeStore<any> = ({ reduxWrapperMiddleware }) =>
+    configureStore({
+        reducer,
+        devTools: true,
+        // middleware: (getDefaultMiddleware) => {
+        //     return getDefaultMiddleware().concat(characterAPI.middleware);
+        // },
+        middleware: (getDefaultMiddleware) =>
+            [
+                ...getDefaultMiddleware(),
+                process.browser ? logger : null,
+                characterAPI.middleware,
+                reduxWrapperMiddleware,
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            ].filter(Boolean) as any,
+    });
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const wrapper = createWrapper<AppStore>(makeStore, { debug: true });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
