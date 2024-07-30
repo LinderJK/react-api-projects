205e3e2b11c5ea766232bf822c8d21946ffc13b6
LinderJK <slepchenkovia@gmail.com>
2024-07-30 22:08:16 +0300

    feat: add HYDRATE action for synchronizing server and client api store

--- class-components/src/services/CharacterService.ts
+++ class-components/src/services/CharacterService.ts
@@ -1,16 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Character, CharacterResponse } from '../types/Character.ts';
+import { HYDRATE } from 'next-redux-wrapper';
+import { Action } from 'redux';
+import { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store/store.ts';
+// import { Action } from 'redux';
+// import { PayloadAction } from '@reduxjs/toolkit';
+// import { RootState } from '../store/store.ts';
+// import { HYDRATE } from 'next-redux-wrapper';
+
+function isHydrateAction(action: Action): action is PayloadAction<RootState> {
+    return action.type === HYDRATE;
+}
 
 export const characterAPI = createApi({
     reducerPath: 'characterAPI',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://rickandmortyapi.com/api/',
     }),
+    extractRehydrationInfo(action, { reducerPath }) {
+        if (isHydrateAction(action)) {
+            return action.payload[reducerPath];
+        }
+    },
     endpoints: (build) => ({
         getCharacterById: build.query<Character, string>({
             query: (id) => `/character/${id}`,
         }),
-        getCharactersByPage: build.query<CharacterResponse, { page: number; name: string }>({
+        getCharactersByPage: build.query<CharacterResponse, { page: string; name: string }>({
             query: ({ page, name }) => `/character?page=${page ?? 1}&name=${name ?? ''}`,
         }),
     }),
