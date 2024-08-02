30b40f807f42c42c69a3838f7ca4e24c3d6b1e0d
LinderJK <slepchenkovia@gmail.com>
2024-08-02 08:53:06 +0300

    refactor: add with and height in image

--- class-components/src/components/TitleBar/TitleBar.tsx
+++ class-components/src/components/TitleBar/TitleBar.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function TitleBar() {
     return (
         <div className={styles.titleBar}>
-            <Image className={styles.logoPortal} src={PortalLogo} alt="portal" />
+            <Image className={styles.logoPortal} src={PortalLogo} width={100} height={100} alt="portal" />
             <h1>Rick & Morty Character Finder</h1>
         </div>
     );
