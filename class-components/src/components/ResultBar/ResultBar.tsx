f3a8ad4be2867a706f4ff4b2cf4bc615dc4b8eac
LinderJK <slepchenkovia@gmail.com>
2024-08-03 18:39:04 +0300

    refactor: delete loader hook

--- class-components/src/components/ResultBar/ResultBar.tsx
+++ class-components/src/components/ResultBar/ResultBar.tsx
@@ -1,19 +1,13 @@
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import styles from './ResultBar.module.css';
 import { Character } from '../../types/Character.ts';
-import useLoader from '../../hooks/useLoader.ts';
 
 interface ResultBarProps {
     results: Character[];
 }
 
 export default function ResultBar({ results }: ResultBarProps) {
-    const { loading } = useLoader();
     const renderData = results.length > 0 ? results : null;
-
-    if (loading) {
-        return <p>Loading...</p>;
-    }
 
     return (
         <div style={{ margin: '20px' }} className={styles.resultBar}>
