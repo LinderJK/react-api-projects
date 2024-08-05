c03c119e0dd92efe739e5599d3fa222291197e97
LinderJK <slepchenkovia@gmail.com>
2024-08-06 00:45:59 +0300

    feat: add character route and component

---
+++ class-components/app/routes/character.tsx
@@ -0,0 +1,9 @@
+import MainPage from '../../src/pages/MainPage/MainPage';
+
+export default function CharacterPage() {
+    return (
+        <div>
+            <MainPage></MainPage>
+        </div>
+    );
+}
