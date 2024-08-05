5f06c386c356422d45113b9c567c8248cebe3ba1
LinderJK <slepchenkovia@gmail.com>
2024-08-05 20:07:02 +0300

    refactor: change character page

--- class-components/src/app/character/page.tsx
+++ class-components/src/app/character/page.tsx
@@ -1,25 +1,3 @@
-import React from 'react';
-import { CharacterResponse } from '../../types/Character.ts';
-import Pagination from '../../components/Pagination/Pagination.tsx';
-import ResultBar from '../../components/ResultBar/ResultBar.tsx';
-
-async function getCharacters(name: string = '', page: string = '1') {
-    const res = await fetch(`https://rickandmortyapi.com/api/character?page=${page}&name=${name}`);
-    const characters: CharacterResponse = await res.json();
-    return characters;
+export default function TestPage() {
+    return null;
 }
-
-async function CharacterPage({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
-    const name = searchParams?.name ?? '';
-    const page = searchParams?.page ?? '1';
-    const data = await getCharacters(name, page);
-
-    return (
-        <React.Fragment>
-            <Pagination maxPages={data.info.pages}></Pagination>
-            <ResultBar results={data.results}></ResultBar>
-        </React.Fragment>
-    );
-}
-
-export default CharacterPage;
