09ba45772c82fce1f5d5b8fa31c55f17f7c238b7
LinderJK <slepchenkovia@gmail.com>
2024-08-06 00:45:12 +0300

    chore: change config for remix

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [remix()],
   test: {
     globals: true,
     environment: 'jsdom',
