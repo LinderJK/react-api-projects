2ed428c70d1379d036dc311b09dd8b9b9a50fb57
LinderJK <slepchenkovia@gmail.com>
2024-08-05 20:04:23 +0300

    refactor: change details layout

--- class-components/src/app/character/details/[id]/layout.tsx
+++ class-components/src/app/character/details/[id]/layout.tsx
@@ -1,12 +1,15 @@
-import CharacterPage from '../../page.tsx';
-import styles from './styles.module.css';
+import styles from '../../styles.module.css';
 
-export default function DetailsLayout({ children }: { children: React.ReactNode }) {
+export default function DetailsPageLayout({
+    children,
+    mainSlot,
+}: {
+    children: React.ReactNode;
+    mainSlot: React.ReactNode;
+}) {
     return (
         <div className={styles.container}>
-            <section className={styles.characterSection}>
-                <CharacterPage></CharacterPage>
-            </section>
+            <section className={styles.characterSection}>{mainSlot}</section>
             <section className={styles.detailsSection}>{children}</section>
         </div>
     );
