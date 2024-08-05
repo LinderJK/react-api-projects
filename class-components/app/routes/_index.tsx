208319ae05e9cc1969fda608a2f1b7d1f73fc1b2
LinderJK <slepchenkovia@gmail.com>
2024-08-06 00:47:56 +0300

    fix: change redirect

--- class-components/app/routes/_index.tsx
+++ class-components/app/routes/_index.tsx
@@ -1,10 +1,6 @@
 import { redirect } from '@remix-run/react';
 import { LoaderFunction } from '@remix-run/node';
 
-export default function App() {
-    return null;
-}
-
 export const loader: LoaderFunction = async () => {
     return redirect(`/character`);
 };
