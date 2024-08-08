5d0f5e33b4268ba64faec3e401e93bbdfc730581
LinderJK <slepchenkovia@gmail.com>
2024-08-08 23:49:50 +0300

    feat: add index file

--- class-components/app/routes/_index.tsx
+++ class-components/app/routes/_index.tsx
@@ -2,5 +2,5 @@ import { redirect } from '@remix-run/react';
 import { LoaderFunction } from '@remix-run/node';
 
 export const loader: LoaderFunction = async () => {
-    return redirect(`/character`);
+    return redirect(`/character?name=&page=1`);
 };
