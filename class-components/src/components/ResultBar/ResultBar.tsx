3bffb1ebd196017f8a1832f51516766c93cf4be8
LinderJK <slepchenkovia@gmail.com>
2024-08-09 00:07:28 +0300

    fix: close details if click left side

--- class-components/src/components/ResultBar/ResultBar.tsx
+++ class-components/src/components/ResultBar/ResultBar.tsx
@@ -1,14 +1,20 @@
 import CharacterCard from '../CharacterCard/CharacterCard.tsx';
 import styles from './ResultBar.module.css';
 import { CharacterResponse } from '../../types/Character.ts';
+import { useLocation, useNavigate } from '@remix-run/react';
 
 interface IResultBarProps {
     data: CharacterResponse;
 }
 
 export default function ResultBar({ data }: IResultBarProps) {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const handleClick = () => {
+        if (location.pathname.includes('/details')) navigate(`/character/${location.search}`);
+    };
     return (
-        <div style={{ margin: '20px' }} className={styles.resultBar}>
+        <div style={{ margin: '20px' }} className={styles.resultBar} onClick={handleClick}>
             {!!data?.results && data.results.length > 0 ? (
                 data.results.map((result) => <CharacterCard key={result.id} character={result}></CharacterCard>)
             ) : (
