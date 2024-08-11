0135dd55cd1c0b13fb891de987c7d8ab06181a8e
LinderJK <slepchenkovia@gmail.com>
2024-08-11 14:30:55 +0300

    chore: remove bundle from coverage

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -1,15 +1,18 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import reactVitest from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [process.env.VITEST ? reactVitest() : remix()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
     coverage: {
       provider: 'v8',
+      include: ['src/**/*.{ts,tsx,js, jsx}'],
+      exclude: ['src/types/*.ts'],
     },
   },
   define: {
