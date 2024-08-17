1f4a5f3e4460a269e9ced3cfc675aad35181267c
LinderJK <slepchenkovia@gmail.com>
2024-08-17 13:10:17 +0300

    feat: add slice in store

--- class-components/src/store/store.tsx
+++ class-components/src/store/store.tsx
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import countrySlice from '../features/countrySlice.ts';
+import uncontrolledSlice from '../features/uncontrolledSlice.ts';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        country: countrySlice,
+        uncontrolled: uncontrolledSlice,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
