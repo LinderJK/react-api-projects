d7e57c953d41b25257eec0aa86e42d10ef2c90e8
LinderJK <slepchenkovia@gmail.com>
2024-08-18 18:43:39 +0300

    refactor: rename components

--- class-components/src/components/CardDeck/CardDeck.tsx
+++ class-components/src/components/CardDeck/CardDeck.tsx
@@ -2,14 +2,20 @@ import { useAppSelector } from '../../hooks/redux.ts';
 import DataCard from '../DataCard/DataCard.tsx';
 import styles from './styles.module.css';
 
-export default function UncontrolledCardDeck() {
-    const data = useAppSelector((state) => state.uncontrolled);
+export default function CardDeck() {
+    const data = useAppSelector((state) => state.forms);
     const isLastItem = (index: number) => index === data.length - 1;
     return (
         <div className={styles.cardDeck}>
             {data.map((item, index) => {
                 const lastItem = isLastItem(index);
-                return <DataCard {...item} key={item.name} isLastItem={lastItem} />;
+                return (
+                    <DataCard
+                        {...item}
+                        key={item.name as string}
+                        isLastItem={lastItem}
+                    />
+                );
             })}
         </div>
     );
--- class-components/src/pages/mainPage.tsx
+++ class-components/src/pages/mainPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import UncontrolledCardDeck from '../components/UncontrolledCardDeck/UncontrolledCardDeck.tsx';
+import CardDeck from '../components/CardDeck/CardDeck.tsx';
 
 export default function MainPage() {
     return (
@@ -13,7 +13,7 @@ export default function MainPage() {
                     <button>Uncontrolled Forms</button>
                 </Link>
             </div>
-            <UncontrolledCardDeck />
+            <CardDeck />
         </div>
     );
 }
